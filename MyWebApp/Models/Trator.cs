2aca69e0fd9eff4057ac4099f1c0a80c0676eb92
Guilherme Azevedo <guilherme30072003@gmail.com>
2023-09-20 17:26:41 -0400

    Criar Trator.cs

---
+++ MyWebApp/Models/Trator.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace MyWebApp.WebSite.Models
+{
+    public class Trator
+    {
+        /*public string Marca { get; set; }
+        public int Ano { get; set; }
+        public string Status { get; set; }
+        public string Descrição { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Trator>(this);*/
+    }
+}
